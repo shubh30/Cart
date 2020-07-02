8114f2b5d464471314d7f338c8c84ec9dcb24bc4
Shubham <shubhamnagpal30@gmail.com>
2020-07-02 12:27:05 +0530

    Add Realtime updates using snapshots

--- src/App.js
+++ src/App.js
@@ -14,11 +14,28 @@ class App extends React.Component{
   }
 
   componentDidMount () {
+    // firebase
+    //   .firestore()
+    //   .collection('products')
+    //   .get()
+    //   .then((snapshot) => {
+    //     const products = snapshot.docs.map((doc) => {
+    //       const data = doc.data();
+
+    //       data['id'] = doc.id;
+    //       return data;
+    //     });
+
+    //     this.setState({
+    //       products,
+    //       loading: false
+    //     })
+    //   })
+
     firebase
       .firestore()
       .collection('products')
-      .get()
-      .then((snapshot) => {
+      .onSnapshot((snapshot) => {
         const products = snapshot.docs.map((doc) => {
           const data = doc.data();
 
